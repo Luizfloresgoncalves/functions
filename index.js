d7d947750971c0d0d5f9b7085c174dff46c0e4da
Luiz Flores Goncalves Neto <luizfloresgoncalvesneto@gmail.com>
2023-08-25 21:53:34 -0300

    criado functions

---
+++ index.js
@@ -0,0 +1,37 @@
+// exercicio 1
+//a. vai aparecer 10 e 50 pois ele fez a variave multiplicado por 5
+//b. ele vai ignorar pois não executa no console
+
+// exercicio2
+//a. essa função faz retornar somente se o usuario colocou cenoura na frase 
+//b. I- vai aparecer true pois aparece cenoura na frase
+//   II- vai aparecer true pois ele primeiro deixa tudo minusculo depois depois inclui cenoura
+//   III- aparece true pois aparece cenouras 
+
+//exercicio 1 codigo de escrita
+/*a. 
+
+    function minhaFuncao1() {
+        const nomeUsuario = prompt("Qual seu nome? ")
+        const idadeUsuario = prompt("Qual sua idade? ")
+        const cidadeUsuario = prompt("Onde vc mora? ")
+        const ocupacaoUsuario = prompt("O que faz da vida? ")
+        console.log(`Eu sou ${nomeUsuario}, tenho ${idadeUsuario} anos, moro em ${cidadeUsuario} e sou ${ocupacaoUsuario}.`)
+    }
+    
+    minhaFuncao1()*/
+
+    /*b. 
+
+    function minhaFuncao2(nomeUsuario, idadeUsuario, cidadeUsuario, profissaoUsuario) {
+    const fraseUsuario = (`Eu sou ${nomeUsuario}, tenho ${idadeUsuario} anos, moro em ${cidadeUsuario} e sou ${profissaoUsuario}.`)
+        return fraseUsuario 
+    }
+    
+    const resultado = minhaFuncao2("Luiz", "39", "São Leopoldo", "estudante")
+    console.log(resultado)*/
+    
+//exercicio 2 codigo de escrita
+//a. 
+
+   
